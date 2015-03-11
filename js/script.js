ea50795d32ec5712388b89d7a0d037e9242d8523
Gabriella <gabcap1@gmail.com>
2015-03-11 16:50:28 -0400

    added legend

--- js/script.js
+++ js/script.js
@@ -9,18 +9,7 @@ var CartoDBTiles = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{
 //add tiles
 fullscreenMap.addLayer(CartoDBTiles);
 
-
-//start with showing the points for one type
-//have a layer defined for each type
-//copy pointLayer for each of these
-//filter dataset to show a given type of category
-//filter through dataset and create an if statement to style it, if not selected set radius to 0
-//for heat map you will have to filter the dataset to pass the dataset in its entirety to heatmap
-//have pre-churned out arrays, and then when someone clicks get heatmap it would get generated
-//    based on what people selected
-//start with 2 layers and see what i can do
-//before heatmap is created would have to clear what's there
-
+//gets Data
 $.getJSON( "geojson/biz.geojson", function( data ) {
 	var dataset = data;
 //density overall
@@ -28,7 +17,7 @@ $.getJSON( "geojson/biz.geojson", function( data ) {
 	gettheHeat(resultAll);
     createBiz(dataset);
 //density by category
-    forEach2(dataset);
+    //forEach2(dataset);
     //gettheHeat2(resultType);
     getControl();
     fullscreenMap.fitBounds(pointLayer.getBounds());
@@ -44,21 +33,13 @@ function createBiz (dataset) {
 	}).addTo(fullscreenMap);
 }
 
-/*var bizPointToLayer = function (feature, latlng){
-	var bizMarker = L.circle(latlng, 200, {
-		stroke: false,
-		fillOpacity: 1
-	});
-	return bizMarker;	
-}*/
-
 var bizStyle = function (feature, latlng) {
     
     var type = feature.properties.Category;
 
     var style = L.circleMarker(latlng, {
         weight: 2,
-        radius: 4,
+        radius: 5,
         opacity: .1,
         color: 'white',
         fillOpacity: 0.75,
@@ -67,62 +48,79 @@ var bizStyle = function (feature, latlng) {
     return style;
 }
 
-function getColor(d) {
-    return d == "Aircraft leasing" ? '#a6cee3' :
-           d == "Education" ? '#1f78b4' :
-           d == "Engineering firms" ? '#b2df8a' :
-           d == "Industrial products" ? '#33a02c' :
-           d == "Logistics support" ? '#fb9a99' :
-           d == "Machine Shops" ? '#e31a1c' :
-           d == "Maintenance, repair and overhaul" ? '#fdbf6f' :
-           d == "Materials distribution" ? '#ff7f00' :
-           d == "Materials processing" ? '#cab2d6' :
-           d == "Other" ? '#6a3d9a' :
-           d == "Parts and equipment distribution" ? '#ffff99' :
-           d == "Parts and equipment manufacturers" ? '#b15928' :
-           d == "Software and IT" ? '#d9d9d9' :
-           d == "Space" ? '#ffed6f' :
-           d == "Test and Calibration" ? '#fccde5' :
-           '#edf8e9';
-}
-
+//creates a pop-up for each company
 var bizClick = function (feature, layer) {
     layer.bindPopup("<strong>Company Name: </strong>" + feature.properties.Comp_Name + "<strong> </br>Category: </strong>" + feature.properties.Category);
 }
 
-//basic for each point: latitude, longitude, count (set all equal to 1)
-//except both the dataset coming in and a category to filter on, could filter with jquery, d3... (js .filter)
-//write a function that creates an array that filters out non-desirable fields
-
+//creates an array to be used in the heatmap function
 function forEach(dataset) {
 	var array = []
 	for (var i = 0; i < dataset.features.length; i++) {
 		array.push([dataset.features[i].properties.Y, dataset.features[i].properties.X, 100]); 
 	}
-    //returns Array [ lat, lng, 100 ]
+    //returns Array [ lat, lng, # ]
 	return array;
 };
 
-//HEAT MAP
-
+//empty variable to create a heatmap layer
 var heatMap;
 
+//draws the heatmap
 function gettheHeat(dataset){
     heatMap = L.heatLayer(dataset, {radius: 10}, {maxZoom: 10}).addTo(fullscreenMap);
 };
 
-function forEach2(dataset) {
-    var categoryArray = []
-    for (var i = 0; i < dataset.features.length; i++) {
-        categoryArray.push([dataset.features[i].properties.Y, dataset.features[i].properties.X, dataset.features[i].properties.Category]); 
-    }
-    //returns Array [ lat, lng, "Category" ]
-    console.log(categoryArray[1]);
-    return categoryArray;
-};
 
 //END FUNCTIONS
 
+//assigns color to company type
+function getColor(d) {
+    return d == "Aircraft leasing" ? '#CC3399' :
+           d == "Education" ? '#FFCC00' :
+           d == "Engineering firms" ? '#00CC99' :
+           d == "Industrial products" ? '#0099FF' :
+           d == "Logistics support" ? '#FF3333' :
+           d == "Machine shops" ? '#FF99FF' :
+           d == "Maintenance, repair and overhaul" ? '#3333FF' :
+           d == "Materials distribution" ? '#FFB894' :
+           d == "Materials processing" ? '#9999FF' :
+           d == "Other" ? '#666666' :
+           d == "Parts and equipment distribution" ? '#CC6600' :
+           d == "Parts and equipment manufacturers" ? '#00A37A' :
+           d == "Software and IT" ? '#CCCC00' :
+           d == "Space" ? '#33CCFF' :
+           d == "Test and calibration" ? '#6a3d9a' :
+           '#edf8e9';
+}
+
+//add legend to map
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'legend');
+        labels = ['<strong>Aerospace Industry by Company Type:</strong>'],
+        categories = ["Aircraft leasing", "Education", "Engineering firms", "Industrial products", "Logistics support", "Machine shops", "Maintenance, repair and overhaul", "Materials distribution", "Materials processing", "Other", "Parts and equipment distribution", "Parts and equipment manufacturers", "Software and IT", "Space", "Test and calibration"];
+
+        for (var i = 0; i < categories.length; i++) {
+                div.innerHTML += 
+                labels.push(
+                    '<i class="circle" style="background:' + getColor(categories[i]) + '"></i> ' +
+                (categories[i] ? categories[i] : '+'));
+            
+            div.innerHTML = labels.join('<br>');
+            div.innerHTML +=
+                '<p></p>' +
+                '<b>Credits:</b>' +
+                '<br><span>Data provided by <a href=\"http://www.locus.org\">Locus Analytics</a><br />' + 
+                'Thanks to <a href=\"http://nijel.org/\">JD Godchaux</a></span><br />';
+};
+    return div;
+    };
+
+legend.addTo(fullscreenMap);
+
+//layer control
 function getControl () {
     
     var baseMaps = {
@@ -130,7 +128,7 @@ function getControl () {
     };
 
     var overlayMaps = {
-        "Heat map": heatMap,        
+        "Overall Density": heatMap,        
         "Companies": pointLayer,
     };
 
@@ -140,6 +138,29 @@ function getControl () {
 
 
 /*
+
+//start with showing the points for one type
+//have a layer defined for each type
+//copy pointLayer for each of these
+//filter dataset to show a given type of category
+//filter through dataset and create an if statement to style it, if not selected set radius to 0
+//for heat map you will have to filter the dataset to pass the dataset in its entirety to heatmap
+//have pre-churned out arrays, and then when someone clicks get heatmap it would get generated
+//    based on what people selected
+//start with 2 layers and see what i can do
+//before heatmap is created would have to clear what's there
+
+
+//
+function forEach2(dataset) {
+    var categoryArray = []
+    for (var i = 0; i < dataset.features.length; i++) {
+        categoryArray.push([dataset.features[i].properties.Y, dataset.features[i].properties.X, dataset.features[i].properties.Category]); 
+    }
+    //returns Array [ lat, lng, "Category" ]
+    console.log(categoryArray[1]);
+    return categoryArray;
+};
 
 //HEAT MAP BY COMPANY TYPE
 
@@ -159,47 +180,6 @@ function gettheHeat2(dataset){
     var heatMap = L.heatLayer(dataset, {radius: 10}, {maxZoom: 10}).addTo(fullscreenMap);
     heatLayer.addLayer(heatMap);
 };
-
-var legend = L.control({position: 'bottomleft'});
-    legend.onAdd = function (map) {
-
-    var div = L.DomUtil.create('div', 'info legend');
-    labels = ['<strong>Categories</strong>'],
-    categories = [Aircraft leasing, Education, Engineering firms, Industrial products, Logistics support, Machine Shops, Maintenance repair and overhaul,
-    Materials distribution, Materials processing, Other, Parts and equipment distribution, Parts and equipment manufacturers, Software and IT, Space,Test and Calibration];
-
-    for (var i = 0; i < dataset.features.length; i++) {
-
-            div.innerHTML += 
-            labels.push(
-                '<i class="circle" style="background:' + getColor(categories[i]) + '"></i> ' +
-            (categories[i] ? categories[i] : '+'));
-
-        }
-        div.innerHTML = labels.join('<br>');
-    return div;
-    };
-    legend.addTo(map);
-
-function getColor(d) {
-    Aircraft leasing = '#a6cee3' :
-    Education = '#1f78b4' :
-    Engineering firms = '#b2df8a' :
-    Industrial products = '#33a02c' :
-    Logistics support = '#fb9a99' :
-    Machine Shops = '#e31a1c' :
-    Maintenance, repair and overhaul = '#fdbf6f' :
-    Materials distribution = '#ff7f00' :
-    Materials processing = '#cab2d6' :
-    Other = '#6a3d9a' :
-    Parts and equipment distribution = '#ffff99' :
-    Parts and equipment manufacturers = '#b15928' :
-    Software and IT = '#d9d9d9' :
-    Space = '#ffed6f' :
-    Test and Calibration = '#fccde5' ;
-}
-
-
 
 ----
 
